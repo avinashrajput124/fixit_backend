37ffde7cd5f5aed57f614942527bd75d0ac0f024
ubuntonaresh <ubuntonaresh@gmail.com>
2023-05-05 09:40:04 +0530

    Add New App For Admin panel

---
+++ AdminPanel/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from AdminPanel import views 
+urlpatterns = [
+    
+]
--- fixit_backend/settings.py
+++ fixit_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,7 +129,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = '/media/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '/home/whosthat/whosthat/staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collect static to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
