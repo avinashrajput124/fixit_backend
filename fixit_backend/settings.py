657413cbb97befff4931883a8f7932a7a82defb8
ubuntonaresh <ubuntonaresh@gmail.com>
2023-05-05 09:44:29 +0530

    Basic setup

--- fixit_backend/settings.py
+++ fixit_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'AdminPanel.apps.AdminpanelConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'technician',
     'user',
+ 
 ]
 
 MIDDLEWARE = [
--- fixit_backend/urls.py
+++ fixit_backend/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('technician/', include('technician.urls')),
-]
+    path('', include('AdminPanel.urls')),
+    
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
