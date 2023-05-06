54a207252a097efaf67c858b176f8a95a83946b6
ubuntonaresh <ubuntonaresh@gmail.com>
2023-05-06 15:44:45 +0530

    styling

--- fixit_backend/settings.py
+++ fixit_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-og%=rapxvbv$*hihi-$c2s_es6@5id-kpr_uqxx0w+ono%@f9d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fixit.space"]
+ALLOWED_HOSTS = ["fixit.space", "127.0.0.1"]
 
 
 # Application definition
